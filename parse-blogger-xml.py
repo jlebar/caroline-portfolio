f3014b10648bc6b785ce6d921f2e966ae47233d0
Justin Lebar <jlebar@google.com>
2019-09-02 16:09:03 -0400

    Some easy escaping.  Draft detection not working yet.

--- parse-blogger-xml.py
+++ parse-blogger-xml.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
+import html
 import sys
 import xml
 from xml.etree import ElementTree as et
 
 NS = {
-  'atom': 'http://www.w3.org/2005/Atom'
+  'atom': 'http://www.w3.org/2005/Atom',
+  'app': 'http://purl.org/atom/app#',
 }
+
+def XmlToMarkdown(x):
+  x = x.replace('<br /><br />', '\n\n')
+  x = x.replace('<br />', '\n\n')
+  x = html.unescape(x)
+  return x
+
 
 tree = et.parse(sys.argv[1])
 root = tree.getroot()
@@ -33,10 +42,22 @@ for entry in root.findall('atom:entry', NS):
   else:
     print('Unknown kind: %s' % kind)
 
-  with open('orig-%ss/%s' % (kind, title.text.replace('/', '-')), 'w') as f:
+  isdraft = False
+  control = entry.find('app:control', NS)
+  if control is not None:
+    draft = entry.find('app:draft', NS)
+    if draft is not None and draft.text == 'yes':
+      isdraft = True
+
+  #for e in entry:
+  #  print(e.tag)
+
+  with open('orig-%ss/%s' % (kind, html.unescape(title.text.replace('/', '-'))), 'w') as f:
     print('+++', file=f)
     print('date = "%s"' % published.text, file=f)
     print('title = "%s"' % title.text, file=f)
     print('tags = %s' % tags, file=f)
+    if isdraft:
+      print('draft = True')
     print('+++', file=f)
-    print(content.text, file=f)
+    print(XmlToMarkdown(content.text), file=f)
