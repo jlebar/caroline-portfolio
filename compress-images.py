4d72b8700e9db01d6fcf1ec9e2c79ee82e399368
Justin Lebar <jlebar@google.com>
2019-12-05 13:53:49 -0500

    More improvements to compress-images.py script.

--- compress-images.py
+++ compress-images.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python3
 
-import os
 import glob
+import multiprocessing
+import os
 import subprocess
+
+from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
 from humanize import naturalsize
 
 # brew install jpeg-archive
+# pip3 install Pillow humanize
+
+THIS_MTIME = os.path.getmtime(__file__)
 
 def compress_image(image, target_size):
+  img_mtime = os.path.getmtime(image)
   outfile_name = (f'{os.path.dirname(os.path.abspath(image))}/'
                   f'.gen.{target_size}.{os.path.basename(image)}')
+  if os.path.exists(outfile_name):
+    out_mtime = os.path.getmtime(outfile_name)
+    if out_mtime > img_mtime and out_mtime > THIS_MTIME:
+      print(f'Skipping {outfile_name}')
 
-  convert_proc = subprocess.run(['convert', image, '-resize', f'{target_size}>x{target_size}>', 'ppm:-'], check=True, capture_output=True)
-  with open('/dev/null', 'w') as dev_null:
-    with open(outfile_name, 'w') as outfile:
-      subprocess.run(['jpeg-recompress', '--method', 'smallfry', '--ppm', '-', '-'],
-                     input=convert_proc.stdout, stdout=outfile, stderr=dev_null, check=True)
+  convert_proc = subprocess.run(['convert', image, '-resize', f'{target_size}>x{target_size}>', 'ppm:-'],
+                                check=True, capture_output=True)
+  with open(outfile_name, 'w') as outfile:
+      subprocess.run(['jpeg-recompress', '-Q', '--quality', 'low', '--method', 'smallfry', '--ppm', '-', '-'],
+                     input=convert_proc.stdout, stdout=outfile, check=True)
   orig_size = os.path.getsize(image)
   new_size = os.path.getsize(outfile_name)
-  print(f'{image}@{target_size} {naturalsize(orig_size)} -> {naturalsize(new_size)} '
+  print(f'{image} @{target_size} {naturalsize(orig_size)} -> {naturalsize(new_size)} '
         f'{orig_size / new_size:.2f}x')
   if new_size >= orig_size:
-    shutil.copyfile(image, outfile_name)
+    os.remove(outfile_name)
 
-images = glob.glob('**/*.jpg', recursive=True)
-for image in images:
-  if image.startswith('resources'):
-    continue
-  for target_size in (1024, 512):
-    compress_image(image, target_size)
+def main():
+  with ThreadPoolExecutor(max_workers=2 * multiprocessing.cpu_count()) as executor:
+    for image in glob.glob('**/*.jpg', recursive=True):
+      if image.startswith('resources'):
+        continue
+
+      # Don't try to upsize an image.
+      img_obj = Image.open(image)
+      max_dim = max(img_obj.width, img_obj.height)
+      sizes = [x for x in [2048, 1536, 1024, 768, 512] if x <= max_dim]
+
+      for target_size in sizes:
+        executor.submit(compress_image, image, target_size)
+
+if __name__ == '__main__':
+  main()
